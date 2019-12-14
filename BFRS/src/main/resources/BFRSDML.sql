ac0945e6bb0a9731be4c099de17669895bb92f11
Dan Kim <dan.dkyu.kim@gmail.com>
2019-12-14 16:37:21 -0500

    modified BFRSDML.sql

--- BFRS/src/main/resources/BFRSDML.sql
+++ BFRS/src/main/resources/BFRSDML.sql
@@ -4,11 +4,7 @@ drop table FORMS cascade constraints;
 drop table TRIP cascade constraints;
 drop table LOGIN_TRIP cascade constraints;
 drop table REVIEW cascade constraints;
-drop table MESSAGE cascade constraints;
-
-drop sequence login_seq;
-drop sequence msg_seq;
-
+​
 create table login (
     id number primary key,
     username varchar2(25),
@@ -24,13 +20,6 @@ create table plane (
     seats number,
     avg_rating number
 );
-create table forms (
-    id number primary key,
-    num_seats number,
-    num_bags number,
-    plane_class varchar2(25),
-    is_round number
-);
 create table trip (
     id number primary key,
     seats_taken number,
@@ -40,14 +29,24 @@ create table trip (
     departure timestamp,
     arrival timestamp,
     price number(7,2),
-    status varchar2(25),
     constraint fk_trip_plane foreign key (plane_id)
         references plane(id)
+);
+create table forms (
+    id number primary key,
+    num_seats number,
+    num_bags number,
+    plane_class varchar2(25),
+    is_round number,
+    trip_id number,
+    constraint fk_forms_trip foreign key (trip_id)
+        references trip(id)
 );
 create table login_trip (
     id number primary key,
     trip_id number,
     login_id number,
+    trip_cost number,
     constraint fk_login_login_trip foreign key (login_id)
         references login(id),
     constraint fk_trip_login_trip foreign key (trip_id)
@@ -60,19 +59,10 @@ create table review (
     constraint fk_review_plane foreign key (plane_id)
         references plane(id)
 );
-create table message (
-    id number primary key,
-    asker_id number,
-    asked_id number,
-    status varchar2(25),
-    type varchar2(25),
-    quest varchar2(256),
-    answer varchar2(256),
-    constraint fk_message_login1 foreign key (asker_id)
-        references login(id),
-    constraint fk_message_login2 foreign key (asked_id)
-        references login(id)
-);
-
-create sequence login_seq;
-create sequence msg_seq;
+​
+drop sequence plane_seq;
+drop sequence trip_seq;
+drop sequence form_seq;
+create sequence plane_seq;
+create sequence trip_seq;
+create sequence form_seq;
