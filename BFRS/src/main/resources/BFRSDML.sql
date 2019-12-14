2fa80e797b5d09b5ac4a603b1b6e735e2d680997
Dan Kim <dan.dkyu.kim@gmail.com>
2019-12-14 17:01:28 -0500

    modified sql again

--- BFRS/src/main/resources/BFRSDML.sql
+++ BFRS/src/main/resources/BFRSDML.sql
@@ -4,6 +4,7 @@ drop table FORMS cascade constraints;
 drop table TRIP cascade constraints;
 drop table LOGIN_TRIP cascade constraints;
 drop table REVIEW cascade constraints;
+drop table MESSAGE cascade constraints;
 
 create table login (
     id number primary key,
@@ -59,9 +60,25 @@ create table review (
     constraint fk_review_plane foreign key (plane_id)
         references plane(id)
 );
+create table message (
+    id number primary key,
+    asker_id number,
+    asked_id number references login(id),
+    status varchar2(25),
+    type varchar2(25),
+    quest varchar2(256),
+    answer varchar2(256),
+    constraint fk_message_login1 foreign key (asker_id)
+        references login(id)
+);
+
+drop sequence login_seq;
+drop sequence msg_seq;
 drop sequence plane_seq;
 drop sequence trip_seq;
 drop sequence form_seq;
+create sequence login_seq;
+create sequence msg_seq;
 create sequence plane_seq;
 create sequence trip_seq;
 create sequence form_seq;
