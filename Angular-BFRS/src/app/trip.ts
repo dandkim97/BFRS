4c38d0fda9e2e91c65d9fc91c6acb729a55627c1
Dan Kim <dan.dkyu.kim@gmail.com>
2019-12-16 13:28:06 -0500

    pull from master correctly this time

---
+++ Angular-BFRS/src/app/trip.ts
@@ -0,0 +1,12 @@
+import { Plane } from './plane';
+
+export class Trip {
+	id: number;
+	seatsTaken: number;
+	tripFrom: string;
+	tripTo: string;
+	departure: string;
+	arrival: string;
+	price: number;
+	plane: Plane;
+}
