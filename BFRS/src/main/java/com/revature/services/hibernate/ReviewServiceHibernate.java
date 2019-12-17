3ca7f790325a1e2e0d52ec3d541abd0bd63803a9
lilac180 <purple.flowers.180@gmail.com>
2019-12-17 13:43:06 -0500

    started on averaging the rates for plane, still need testing

--- BFRS/src/main/java/com/revature/services/hibernate/ReviewServiceHibernate.java
+++ BFRS/src/main/java/com/revature/services/hibernate/ReviewServiceHibernate.java
@@ -3,7 +3,9 @@ package com.revature.services.hibernate;
 import org.springframework.stereotype.Service;
 
 import com.revature.beans.Review;
+import com.revature.data.PlaneDao;
 import com.revature.data.ReviewDao;
+import com.revature.data.hibernate.PlaneHibernate;
 import com.revature.data.hibernate.ReviewHibernate;
 import com.revature.services.ReviewService;
 
@@ -11,10 +13,15 @@ import com.revature.services.ReviewService;
 public class ReviewServiceHibernate implements ReviewService{
 	
 	private ReviewDao rd = new ReviewHibernate();
+	private PlaneDao pd = new PlaneHibernate();
 
 	@Override
 	public void addReview(Review r) {
 		rd.addReview(r);
+		Integer currRating = r.getPlane().getAvgRating();
+		Integer newRating = (currRating + r.getRating()) / 2;
+		r.getPlane().setAvgRating(newRating);
+		pd.updatePlane(r.getPlane());
 		
 	}
 
