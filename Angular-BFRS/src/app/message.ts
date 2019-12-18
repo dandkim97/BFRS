297eac6995ee6ae4e29da466763d68a9bd9ada61
Dan Kim <dan.dkyu.kim@gmail.com>
2019-12-18 10:56:26 -0500

    nuke

--- Angular-BFRS/src/app/message.ts
+++
@@ -1,9 +0,0 @@
-export class Message {
-    id: number;
-    askerId: number;
-    askedId: number;
-    status: string;
-    type: string;
-    quest: string;
-    answer: string;
-}
--- Angular-BFRS/src/app/plane.spec.ts
+++
@@ -1,7 +0,0 @@
-import { Plane } from './plane';
-
-describe('Plane', () => {
-  it('should create an instance', () => {
-    expect(new Plane()).toBeTruthy();
-  });
-});
